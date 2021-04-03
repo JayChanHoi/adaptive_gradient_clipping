e3848aa77d09a59382730349f983d9674b9dea0b
Jay Chan <34962937+chan111222333@users.noreply.github.com>
2021-04-03 17:37:20 +0800

    update

--- exp.py
+++ exp.py
@@ -53,7 +53,7 @@ def train(model_name='v1_nf1_gelu_agc'):
         model = torch.nn.DataParallel(model)
 
     print(model.state_dict().keys())
-    optimizer = AGC(torch.optim.Adam(model.parameters(), lr=0.0001, weight_decay=0.00001), clip_lambda=0.01, layer_to_skip=['fc'])
+    optimizer = AGC(torch.optim.Adam(model.parameters(), lr=0.0001, weight_decay=0.00004), clip_lambda=0.01, layer_to_skip=['fc'])
     loss_func = torch.nn.CrossEntropyLoss()
     model.train()
 
