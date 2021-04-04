8a76c258b78eb34c0c07495ea8dd0a0db0ff0e5a
Jay Chan <34962937+chan111222333@users.noreply.github.com>
2021-04-04 14:45:37 +0800

    update

--- exp.py
+++ exp.py
@@ -31,8 +31,8 @@ def train(model_name='v1_efficient_net_b0_agc'):
         torchvision.transforms.ToTensor(),
         torchvision.transforms.Lambda(lambd=lambda x: x.repeat(3, 1, 1)),
         torchvision.transforms.Resize(model_dict['0']),
-        torchvision.transforms.RandomHorizontalFlip(),
-        torchvision.transforms.RandomVerticalFlip(),
+        # torchvision.transforms.RandomHorizontalFlip(),
+        # torchvision.transforms.RandomVerticalFlip(),
         # torchvision.transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
     ])
 
